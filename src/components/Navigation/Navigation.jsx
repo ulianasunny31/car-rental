ea68ea32fcf71bb1e900263a6ce55711fd5c7ca3
Uliana Seredenko <uliana1331@gmail.com>
2025-05-14 10:05:18 +0200

    add navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import css from './Navigation.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+const Navigation = () => {
+  return (
+    <div className={css.navigation}>
+      <h3>
+        Rental
+        <span>Car</span>
+      </h3>
+
+      <div>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/catalog" className={buildLinkClass}>
+          Catalog
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
