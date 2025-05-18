4c9bb4e789bc9872f9f21a8bc7cdb4347d36e516
Uliana Seredenko <uliana1331@gmail.com>
2025-05-18 11:45:11 +0200

    change loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,10 @@
 import css from './Loader.module.css';
-import { Hourglass } from 'react-loader-spinner';
+import { ClockLoader } from 'react-spinners';
 
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <Hourglass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="hourglass-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        colors={['#306cce', '#72a1ed']}
-      />
+      <ClockLoader color="#3470ff" />
     </div>
   );
 };
