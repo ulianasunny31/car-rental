3f4c43556bf0a3f0b34babc03d7c93602afc722c
Uliana Seredenko <uliana1331@gmail.com>
2025-05-18 11:34:56 +0200

    add loader comp

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,20 @@
+import css from './Loader.module.css';
+import { Hourglass } from 'react-loader-spinner';
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={css.loader}>
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={['#306cce', '#72a1ed']}
+      />
+    </div>
+  );
 };
 
 export default Loader;
